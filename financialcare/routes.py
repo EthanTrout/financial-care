5ed22d558f7d845beafc2729c5e6e8e373565586
Ethan Trout <ethan-james-trout@outlook.com>
2024-07-29 18:12:08 +0000

    feat: Added view services and add services. implemented POST in routes to update services table in database.

--- financialcare/routes.py
+++ financialcare/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from financialcare import app, db
 from financialcare.models import User,Service , user_service
 
@@ -7,6 +7,15 @@ from financialcare.models import User,Service , user_service
 def login():
     return render_template("login.html")
 
-@app.route("/home")
-def home():
-    return render_template("base.html")
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/add_service",methods=["GET","POST"])
+def add_service():
+    if request.method=="POST":
+        service = Service(name=request.form.get("service_name"))
+        db.session.add(service)
+        db.session.commit()
+        return redirect(url_for("services"))
+    return render_template("add_service.html")
