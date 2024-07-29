bcfd9cda828810985bb044c812e219936ab17d34
Ethan Trout <ethan-james-trout@outlook.com>
2024-07-29 17:25:46 +0000

    feat: added base template for nav and footer to be displayed on all pages

--- financialcare/routes.py
+++ financialcare/routes.py
@@ -6,3 +6,7 @@ from financialcare.models import User,Service , user_service
 @app.route("/")
 def login():
     return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("base.html")
---
+++ financialcare/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Side Nav intialisation
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
